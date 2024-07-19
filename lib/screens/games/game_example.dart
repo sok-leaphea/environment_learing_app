2d2a6a110f663bb33b635b8de86435800b43c904
sok-leaphea <leapheas2@gmail.com>
2024-07-19 09:53:45 +0700

    add-drag-and-drop-game

--- lib/screens/games/game_example.dart
+++ lib/screens/games/game_example.dart
@@ -8,35 +8,76 @@ class GameExample extends StatefulWidget {
 }
 
 class _GameExampleState extends State<GameExample> {
+  bool _isDropped = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Draggable<String>(
-              // Data is the value this Draggable stores.
-              data: 'red',
-              feedback: SizedBox(
-                height: 120.0,
-                width: 120.0,
-                child: Center(
-                  child: Image.asset('assets/images/recycle.png'),
+      body: Container(
+        padding: const EdgeInsets.all(40.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Draggable<String>(
+                data: 'true',
+                feedback: SizedBox(
+                  height: 120.0,
+                  width: 120.0,
+                  child: Center(
+                    child: Image.asset('assets/images/recycle.png'),
+                  ),
+                ),
+                childWhenDragging: Container(),
+                child: SizedBox(
+                  height: 120.0,
+                  width: 120.0,
+                  child: Center(
+                    child: Image.asset('assets/images/recycle.png'),
+                  ),
                 ),
               ),
-              child: SizedBox(
-                height: 120.0,
-                width: 120.0,
-                child: Center(
-                  child: Image.asset('assets/images/recycle.png'),
-                ),
+              DragTarget<String>(
+                builder: (
+                    BuildContext context,
+                    List<dynamic> accepted,
+                    List<dynamic> rejected,
+                    ) {
+                  return SizedBox(
+                    height: 120.0,
+                    width: 120.0,
+                    child: Center(
+                      child: Image.asset(_isDropped
+                          ? 'assets/images/protect.png'
+                          : 'assets/images/smoke.png'),
+                    ),
+                  );
+                },
+                onWillAccept: (data) {
+                  return data == 'true';
+                },
+                onAccept: (data) {
+                  setState(() {
+                    _isDropped = true;
+                  });
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('True!'),
+                    ),
+                  );
+                },
+                onLeave: (data) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('False!'),
+                    ),
+                  );
+                },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
